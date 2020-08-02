430c0d57093ff648050fecbd21d204c99436b5bf
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-02 17:24:32 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 10) {
+        kitronik_servo_lite.backward()
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        basic.showArrow(ArrowNames.North)
+    }
+    if (receivedNumber == 20) {
+        kitronik_servo_lite.forward()
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        basic.showArrow(ArrowNames.South)
+    }
+    if (receivedNumber == 30) {
+        kitronik_servo_lite.turnLeft(90)
+        strip.show()
+        basic.showArrow(ArrowNames.West)
+    }
+    if (receivedNumber == 40) {
+        kitronik_servo_lite.turnRight(90)
+        strip.show()
+        basic.showArrow(ArrowNames.East)
+    }
+    if (receivedNumber == 50) {
+        kitronik_servo_lite.stop()
+        basic.showIcon(IconNames.No)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(10)
+    basic.showArrow(ArrowNames.North)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(30)
+    basic.showArrow(ArrowNames.West)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(50)
+    basic.showIcon(IconNames.No)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(20)
+    basic.showArrow(ArrowNames.South)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(40)
+    basic.showArrow(ArrowNames.East)
+})
+let strip: neopixel.Strip = null
+radio.setGroup(10)
+strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+strip.show()
+basic.showIcon(IconNames.Heart)
